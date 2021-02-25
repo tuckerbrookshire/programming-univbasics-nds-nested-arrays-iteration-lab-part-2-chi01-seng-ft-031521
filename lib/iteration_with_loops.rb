724ecd2f9f44b1f397c0d691e5b0cc0b6df5231a
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-25 02:25:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ require "pry"
 def find_min_in_nested_arrays(src)
   even_src = []
   row_index = 0
-  min_temp_value = nil
+  min_temp_value = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
